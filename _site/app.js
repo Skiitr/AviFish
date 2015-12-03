2cd3792815f6ab29edec91e7254399f7023b972e
Dan Gallagher <dangallagher@Dans-MacBook-Pro.local>
2015-12-02 21:07:49 -0500

    First working version

--- _site/app.js
+++ _site/app.js
@@ -9,19 +9,22 @@
 var lincolnButton = document.getElementById("dp-lincoln");
 var melbourneButton = document.getElementById("dp-melbourne");
 var openButton = document.getElementById("cl-open");
-var clearClButton = document.getElementById("cl-clear");
-var selectallButton = document.getElementById("rs-selectall");
-var clearRsButton = document.getElementById("rs-clear");
+//var clearClButton = document.getElementById("cl-clear");
+//var selectallButton = document.getElementById("rs-selectall");
+//var clearRsButton = document.getElementById("rs-clear");
 var textOptionButton = document.getElementById("op-text");
-var clOptionButton = document.getElementById("op-changelist");
-var verOptionButton = document.getElementById("op-version");
+var xmlOptionButton = document.getElementById("op-xml");
 var translateButton = document.getElementById("in-translate");
-var clearInButton = document.getElementById("in-clear")
+//var clearInButton = document.getElementById("in-clear")
 
-//Collect the DOM Input and Output into variables
-var changelistInput = document.getElementById("cl-input");
+//Container for the resutlt list
+var resultsList = document.createElement("ul");
+resultsList.id = "rs-resultsList"
 
-var resultsOutput = document.getElementById("rs-results");
+//Depot, Option and Changelist items
+var depot = "http://aviapp.avidyne.com:8080/";
+var option = "?ac=160";
+var changelist = "";
 
 //Toggle the depot to select
 function depotToggle(depot) {
@@ -33,87 +36,54 @@ function depotToggle(depot) {
 //Toggle the option to select
 function optionToggle(option) {
   textOptionButton.setAttribute("class", "button");
-  clOptionButton.setAttribute("class", "button");
-  verOptionButton.setAttribute("class", "button");
+  xmlOptionButton.setAttribute("class", "button");
   option.setAttribute("class", "button selected");
 }
 
 //Toggles for each button:
 lincolnButton.onclick = function() {
   depotToggle(lincolnButton);
+  repository = "http://aviapp.avidyne.com:8080/";
 };
 melbourneButton.onclick = function() {
   depotToggle(melbourneButton);
+  repository = "http://palmtree.avidyne.com:8080/";
 };
 textOptionButton.onclick = function() {
   optionToggle(textOptionButton);
+  option = "?ac=160";
 };
-clOptionButton.onclick = function() {
-  optionToggle(clOptionButton);
-};
-verOptionButton.onclick = function() {
-  optionToggle(verOptionButton);
+xmlOptionButton.onclick = function() {
+  optionToggle(xmlOptionButton);
+  option = "?ac=201";
 };
 
 //Build the link to fetch
 function buildUrl(changelist, repository, option) {
   var fetchUrl = repository;
-  fetchUrl += "@md=d&cd=//&c=5a1&rc=s@/p4%20describe%20";
+  fetchUrl += "@md=d&cd=//&c=&rc=s@/p4%20describe%20";
   fetchUrl += changelist;
   fetchUrl += option;
   return fetchUrl;
 }
 
-//Set the results to the Results box
-function setResults(data) {
-  resultsOutput.value = data;
+//Add to results list for each item
+function addToResults(linkMeat, depot, changelist) {
+  var itemToAdd = document.createElement("li");
+  itemToAdd.innerHTML = depot + linkMeat + "@" + changelist;
+  resultsList.appendChild(itemToAdd);
 }
-
-var windowHTML = "";
 
 //On open click collect data and open URL
 openButton.onclick = function() {
   console.log("Open Button Clicked");
-
-  var depot = "";
-  var option = "";
-  var changelist = "";
-
-  //Set Depot Selected
-  if (lincolnButton.getAttribute("class") === "button selected") {
-    console.log("Lincoln Depot Selected");
-    repository = "http://aviapp.avidyne.com:8080/";
-  } else if (melbourneButton.getAttribute("class") === "button selected") {
-    console.log("Melbourne Depot Selected");
-    repository = "http://palmtree.avidyne.com:8080/";
-  } else {
-    console.log("Errrror")
-  }
-  
-  //Set Option Selected
-  if (textOptionButton.getAttribute("class") === "button selected") {
-    console.log("Text Option Selected");
-    option = "?ac=160";
-  } else if (clOptionButton.getAttribute("class") === "button selected") {
-    console.log("Changelist Links Option Selected");
-    option = "?ac=201";
-  } else if (verOptionButton.getAttribute("class") === "button selected") {
-    console.log("Version Links Option Selected");
-    option = "?ac=201";
-  } else {
-    console.log("Errrror")
-  }
   
   //Set Changelist Entered
-  if (changelistInput != "") {
-    console.log("Changelist Number: " + changelistInput.value);
-    changelist = changelistInput.value;
-  } else {
-    console.log("Errrror")
-  }
+  var changelistInput = document.getElementById("cl-input");
+  changelist = changelistInput.value;
   
   //Build the url using selected options
-  var xmlUrl = buildUrl(changelist, repository, option);
+  var xmlUrl = buildUrl(changelist, depot, option);
   console.log("The xmlUrl is " + xmlUrl);
 
   window.open(xmlUrl);
@@ -123,9 +93,30 @@ openButton.onclick = function() {
 translateButton.onclick = function() {
   console.log("Translate Button Clicked");
 
-  var inputText = document.getElementById("in-inputArea");
-  console.dir(inputText.typeOf);
+  //Get results box item
+  var resultsDiv = document.getElementById("rs-results");
+  //Clear results box
+  resultsList.innerHTML = "";
+  resultsDiv.innerHTML = "";
+
+  //Grab the XML from the input and parse to XML
+  var inputXML = $.parseXML(document.getElementById("in-inputArea").value);
+  
+  //Extract the depotfiles from the XML data
+  var depotFiles = inputXML.getElementsByTagName("depotfile")
+
+  //Lopp through depotFiles and pull out the link values
+  for (var i = 0; i < depotFiles.length; i++) {
+    var linkValue = depotFiles[i].getAttributeNode("value");
+    addToResults(linkValue.value, depot, changelist);
+  };
+
+  //Add resultsList to results box
+  resultsDiv.appendChild(resultsList);
+  
 }
+
+
 
 
 
